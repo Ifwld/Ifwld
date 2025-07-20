4164b0732d6d48a40dd11dbdc2ee624b11abd9cb
Ifwld <vinlandppp@gmail.com>
2025-07-20 13:50:21 +0300

    Update rabbitsim.lua

--- rabbitsim.lua
+++ rabbitsim.lua
@@ -4,7 +4,7 @@
 -- Instances:
 
 local ScreenGui = Instance.new("ScreenGui")
-local ScrollingFrame = Instance.new("ScrollingFrame")
+local Frame = Instance.new("Frame")
 local TextButton = Instance.new("TextButton")
 local TextButton_2 = Instance.new("TextButton")
 
@@ -13,32 +13,60 @@ local TextButton_2 = Instance.new("TextButton")
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-ScrollingFrame.Parent = ScreenGui
-ScrollingFrame.Active = true
-ScrollingFrame.BackgroundColor3 = Color3.fromRGB(66, 0, 0)
-ScrollingFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ScrollingFrame.BorderSizePixel = 0
-ScrollingFrame.Position = UDim2.new(0.251034439, 0, 0.237864077, 0)
-ScrollingFrame.Size = UDim2.new(0, 355, 0, 195)
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(66, 0, 0)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.172413796, 0, 0.187702283, 0)
+Frame.Size = UDim2.new(0, 423, 0, 209)
 
-TextButton.Parent = ScrollingFrame
-TextButton.BackgroundColor3 = Color3.fromRGB(66, 0, 0)
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(200, 0, 0)
 TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.216901407, 0, 0.051282052, 0)
-TextButton.Size = UDim2.new(0, 200, 0, 34)
+TextButton.Position = UDim2.new(0.929078043, 0, 0, 0)
+TextButton.Size = UDim2.new(0, 29, 0, 27)
 TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "AUTOCARROT"
+TextButton.Text = "X"
 TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.TextSize = 25.000
 
-TextButton_2.Parent = ScrollingFrame
-TextButton_2.BackgroundColor3 = Color3.fromRGB(66, 0, 0)
+TextButton_2.Parent = Frame
+TextButton_2.BackgroundColor3 = Color3.fromRGB(29, 121, 118)
 TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextButton_2.BorderSizePixel = 0
-TextButton_2.Position = UDim2.new(0.216901407, 0, 0.451282054, 0)
-TextButton_2.Size = UDim2.new(0, 200, 0, 35)
+TextButton_2.Position = UDim2.new(0.0236406624, 0, 0.0526315793, 0)
+TextButton_2.Size = UDim2.new(0, 115, 0, 40)
 TextButton_2.Font = Enum.Font.SourceSans
-TextButton_2.Text = "AUTOREBIRTH"
+TextButton_2.Text = "InfiniteCarrot"
 TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.TextSize = 25.000
+TextButton_2.TextSize = 14.000
+
+-- Scripts:
+
+local function GUWC_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	local ScreenGui = script.Parent.Parent -- Bu kodun ScreenGui'nin içinde bir yerde olduğunu varsayıyoruz
+	
+	-- Eğer bu script bir TextButton'ın içine konulmuşsa:
+	local textButton = script.Parent
+	
+	textButton.MouseButton1Click:Connect(function()
+		ScreenGui:Destroy()
+		print("ScreenGui yok edildi!")
+	end)
+end
+coroutine.wrap(GUWC_fake_script)()
+local function DAEOIX_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
+
+	local textButton = script.Parent
+	
+	textButton.MouseButton1Click:Connect(function()
+		while wait() do
+			game.Players.LocalPlayer.Backpack.Carrot.E:FireServer()
+		end
+	end)
+end
+coroutine.wrap(DAEOIX_fake_script)()
